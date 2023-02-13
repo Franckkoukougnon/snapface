67f349c8419df239fd889ad8b3e0f114f00a39a5
franck <franckkoukougnon@yahoo.fr>
2023-02-14 00:33:25 +0100

    unsubscribe

--- src/app/component/face-snap-list/face-snap-list.component.ts
+++ src/app/component/face-snap-list/face-snap-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subject, take, takeUntil, tap } from 'rxjs';
 import { FaceSnap } from '../../models/face-snap.models';
 import { FaceSnapService } from '../../services/face-snap.service';
 
@@ -7,12 +8,22 @@ import { FaceSnapService } from '../../services/face-snap.service';
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss'],
 })
-export class FaceSnapListComponent implements OnInit {
-  @Input() faceSnaps!: FaceSnap[];
+export class FaceSnapListComponent implements OnInit, OnDestroy {
+  @Input() faceSnaps: FaceSnap[] | undefined;
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapService: FaceSnapService) {}
 
   ngOnInit() {
     this.faceSnaps = this.faceSnapService.getAllFaceSnap();
+    this.destroy$ = new Subject<boolean>();
+
+    interval(1000)
+      .pipe(take(10), tap(console.log), takeUntil(this.destroy$))
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
   }
 }
